16b1406b5062694d2515fc5ddb2ae2bfaeb6de54
Isabel Robleda <isabel.robleda@gmail.com>
2024-03-18 15:38:18 +0100

    first components created

---
+++ src/components/ToDoHeader.vue
@@ -0,0 +1,63 @@
+<template>
+    <header>
+        <nav class="container">
+            <div class="branding">
+                <img src="../assets/Vue_Logo_Black.png" alt="">
+                <h1>Vue To-Dos</h1>
+            </div>
+            <ul class="nav-routes">
+                <RouterLink to="/">
+                    Home
+                </RouterLink>
+                <RouterLink to="/about">
+                    About
+                </RouterLink>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'ToDoHeader'
+}
+</script>
+
+<style scoped lang="scss">
+header {
+    background-color: #f1f1f1;
+
+    nav {
+        display: flex;
+        align-items: center;
+        padding: 25px 16px;
+
+        .branding {
+            display: flex;
+            align-items: center;
+            gap: 8px;
+
+            img {
+                max-width: 50px;
+            }
+
+            h1 {
+                font-size: 24px;
+            }
+        }
+
+        .nav-routes {
+            display: flex;
+            flex: 1;
+            justify-content: flex-end;
+            gap: 12px;
+            list-style: none;
+
+            a {
+                text-decoration: none;
+                color: inherit;
+            }
+        }
+    }
+}
+</style>
